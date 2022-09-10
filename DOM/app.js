727b7672565cbd86ea3a8aad2e87f6367366012a
KobyGitHub <103534700+KobyGitHub@users.noreply.github.com>
2022-09-10 15:30:41 -0500

    Add files via upload

---
+++ DOM/app.js
@@ -0,0 +1,21 @@
+// const section = document.getElementById('container');
+// const sectionId = document.querySelector('#container');
+// const li = document.querySelectorAll('.second');
+// const ol = document.querySelectorAll('ol .third');
+// const hello = document.querySelector('#container');
+// hello.textContent = 'Hello!'
+
+// const classMain = document.querySelectorAll('div');
+// const footerClass = classMain[1];
+// footerClass.classList.add('main'); 
+// footerClass.classList.remove('main');
+
+// const newLi = document.createElement('li');
+// newLi.textContent = 'four';
+// const ul = document.querySelector('ul');
+// ul.append('li');
+
+// const divFooter = document.querySelectorAll('div')[1];
+// divFooter.remove();
+
+
